98167018446f167f73032f70b6bece24185f7ee0
awadyn <awadyn@bu.edu>
2024-06-23 12:18:09 -0400

    script to setup and start intlogger

---
+++ cloudlab_setup_intlogger.sh
@@ -0,0 +1,57 @@
+node=$1
+myself="awadyn"
+
+#ssh $myself@$node 'sudo apt update && sudo apt upgrade -y'
+
+kernel=$(ssh $myself@$node 'uname -r')
+if [[ $kernel != "5.15.89" ]]; 
+then 
+echo "Kernel version $kernel is bad for building intlogger. Installing 5.15.89 instead.";
+ssh $myself@$node 'sudo apt install -y fakeroot dwarves flex bison libssl-dev libelf-dev'
+ssh $myself@$node 'wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.89.tar.xz; 
+	tar -xf linux-5.15.89.tar.xz; 					\
+	cd linux-5.15.89; 						\
+	cp -v /boot/config-$(uname -r) .config;  			\
+	make localmodconfig;						\
+	scripts/config --disable SYSTEM_TRUSTED_KEYS; 			\
+	scripts/config --disable SYSTEM_REVOCATION_KEYS; 		\
+	scripts/config --set-str CONFIG_SYSTEM_TRUSTED_KEYS ""; 	\
+	scripts/config --set-str CONFIG_SYSTEM_REVOCATION_KEYS ""; 	\
+	fakeroot make -j8;						\
+	sudo make modules_install;					\
+	sudo make install;						\
+	sudo reboot'
+sleep 30
+fi
+
+
+echo "Kernel version $kernel found. Installing intlogger.."
+ssh $myself@$node 'sudo rmmod ixgbe;							\
+	if [ -d intlog ]; then echo "intlog source exists.. skipping clone.."; 		\
+	else git clone https://github.com/handong32/intlog.git;	fi;			\
+	linux_dir="linux-5.15.89/drivers/net/ethernet/intel/ixgbe";			\
+	intlog_dir="intlog/linux/"$linux_dir;						\
+	delta=$(cmp $linux_dir/ixgbe_main.c $intlog_dir/ixgbe_main.c);			\
+	if [ $delta ]; then 								\
+	echo "linux ixgbe and intlog ixgbe differ"; 					\
+	cp -r ~/intlog/linux/linux-5.15.89/drivers/net/ ~/linux-5.15.89/drivers/; 	\
+	cd linux-5.15.89; 								\
+	fakeroot make -j8; fi';
+
+echo "Disabling hyperthreads.."
+ssh $myself@$node 'echo off | sudo tee /sys/devices/system/cpu/smt/control'
+#
+echo "Starting intlogger.."
+ssh $myself@$node 'sudo insmod ~/linux-5.15.89/drivers/net/ethernet/intel/ixgbe/ixgbe.ko;			\
+	ieth=$(sudo dmesg | grep "ixgbe" | grep "renamed from eth0" | tail -n 2 | head -n 1 | cut -d " " -f4);	\
+	ieth=${ieth::-1};											\
+	ip=$(ip addr | grep -B1 128.110.96 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)");		\
+	echo "intlogger network interface: $ieth";								\
+	echo "intlogger network ip: $ip";									\
+	sudo ip link set dev $ieth up;										\
+	sudo ip addr add $ip dev $ieth'
+
+echo "Testing intlogger.."
+ssh $myself@$node 'for i in {0..15}; do cat /proc/ixgbe_stats/core/$i; echo; done'
+
+
