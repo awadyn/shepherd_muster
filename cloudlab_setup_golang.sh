952299d249d3f905bc415ec317e6b9aceb688614
awadyn <awadyn@node0.mustherd-doc.bayopsys-pg0.apt.emulab.net>
2024-10-02 19:24:53 +0000

    Basic golang setup for MustHerd; TODO: discovering mismatching go versions + deleting old + installing new

---
+++ cloudlab_setup_golang.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+go_version="1.22.4"
+bin_dir=/usr/local/
+
+echo "Setting up go$go_version:"
+sleep 1
+if [[ $(which go) ]]; 
+then echo "golang found at:"; which go; go version; sleep 1;
+else echo "golang not found.. Downloading go$go_version:"; sleep 1; 
+	wget https://go.dev/dl/go$go_version.linux-amd64.tar.gz; 
+	sudo tar -C $bin_dir -xzf go$go_version.linux-amd64.tar.gz;	
+	echo "export PATH=$PATH:$bin_dir/go/bin" >> .bashrc;
+	export PATH=$PATH:$bin_dir/go/bin;
+	which go;
+	go version;
+fi
+
