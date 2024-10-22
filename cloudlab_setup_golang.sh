0d2154f098e6d5e5d6bf90b645019941936bd49c
awadyn <awadyn@node0.mustherd-doc.bayopsys-pg0.apt.emulab.net>
2024-10-22 18:28:19 +0000

    removed faulty env variable

--- cloudlab_setup_golang.sh
+++ cloudlab_setup_golang.sh
@@ -11,7 +11,6 @@ else echo "golang not found.. Downloading go$go_version:"; sleep 1;
 	wget https://go.dev/dl/go$go_version.linux-amd64.tar.gz; 
 	sudo tar -C $bin_dir -xzf go$go_version.linux-amd64.tar.gz;	
 	echo "export PATH=$PATH:$bin_dir/go/bin" >> ~/.bashrc;
-	export PATH=$PATH:$bin_dir/go/bin;
 	which go; go version;
 fi
 
