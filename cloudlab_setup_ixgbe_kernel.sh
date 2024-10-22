225dee5f64303333c6575356ee45ccf28bc245cd
awadyn <awadyn@node1.mustherd-doc.bayopsys-pg0.apt.emulab.net>
2024-10-22 19:06:00 +0000

    decomposed ixgbe driver setup

---
+++ cloudlab_setup_ixgbe_kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ~/
+kernel=$(uname -r)
+if [[ $kernel != "5.15.89" ]]; 
+then 
+echo "Kernel version $kernel is bad for building intlogger. Installing 5.15.89 instead."
+sleep 2
+sudo apt install -y fakeroot dwarves flex bison libssl-dev libelf-dev
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.89.tar.xz
+tar -xf linux-5.15.89.tar.xz
+cd linux-5.15.89
+cp -v /boot/config-$(uname -r) .config 
+make localmodconfig
+scripts/config --disable SYSTEM_TRUSTED_KEYS
+scripts/config --disable SYSTEM_REVOCATION_KEYS
+scripts/config --set-str CONFIG_SYSTEM_TRUSTED_KEYS ""
+scripts/config --set-str CONFIG_SYSTEM_REVOCATION_KEYS ""
+fakeroot make -j8
+sudo make modules_install
+sudo make install
+sudo reboot
+fi
+
--- cloudlab_setup_ixgbe_logger.sh
+++ cloudlab_setup_ixgbe_logger.sh
@@ -1,32 +1,20 @@
-kernel=$(uname -r)
-cd ~/
-if [[ $kernel != "5.15.89" ]]; 
-then 
-echo "Kernel version $kernel is bad for building intlogger. Installing 5.15.89 instead."
-sleep 2
-sudo apt install -y fakeroot dwarves flex bison libssl-dev libelf-dev
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.89.tar.xz
-tar -xf linux-5.15.89.tar.xz
-cd linux-5.15.89
-cp -v /boot/config-$(uname -r) .config 
-make localmodconfig
-scripts/config --disable SYSTEM_TRUSTED_KEYS
-scripts/config --disable SYSTEM_REVOCATION_KEYS
-scripts/config --set-str CONFIG_SYSTEM_TRUSTED_KEYS ""
-scripts/config --set-str CONFIG_SYSTEM_REVOCATION_KEYS ""
-fakeroot make -j8
-sudo make modules_install
-sudo make install
-sudo reboot
-sleep 30
-fi
+#!/bin/bash
 
+cd ~/
+kernel=$(uname -r)
 echo "Kernel version $kernel found. Installing intlogger.."
 sleep 2
+if [ ! -d "~/intlog" ]; then
 git clone https://github.com/handong32/intlog.git
 cp -r ~/intlog/linux/linux-5.15.89/drivers/net/ ~/linux-5.15.89/drivers/
 cd linux-5.15.89
 fakeroot make -j8
+fi
+
+sleep 2
+echo "Disabling hyperthreads and turboboost.."
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 
 sleep 2
 echo "Loading ixgbe interrupt logger.."
@@ -39,17 +27,12 @@ ip="10.10.1.$node"
 echo "ixgbe interrupt logger interface: $ieth"
 echo "ixgbe interrupt logger ip: $ip"
 sudo ip link set dev $ieth up
-sudo ip addr add $ip dev $ieth
+sudo ip addr add $ip/24 dev $ieth
 
 sleep 2
 echo "Disabling irqbalance and setting irq affinity.."
 sudo killall irqbalance
 sudo ~/shepherd_muster/intel_set_irq_affinity.sh $ieth
-
-sleep 2
-echo "Disabling hyperthreads and turboboost.."
-echo off | sudo tee /sys/devices/system/cpu/smt/control
-echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 
 sleep 2
 echo "Testing intlogger.."
