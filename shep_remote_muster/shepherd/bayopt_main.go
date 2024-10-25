0cdc59d8177ff031cb2d7a4da5190d1cbc9f9d51
awadyn <awadyn@node0.mustherd-doc.bayopsys-pg0.apt.emulab.net>
2024-10-25 01:43:45 +0000

    successful bayopt shepherd test with mutilate load generator for memcached

--- shep_remote_muster/shepherd/bayopt_main.go
+++ shep_remote_muster/shepherd/bayopt_main.go
@@ -2,7 +2,7 @@ package main
 
 import ( "time"
 	 "os"
-//	 "os/exec" 
+	 "os/exec" 
 )
 
 /**************************************/
@@ -31,10 +31,12 @@ func (bayopt_s *bayopt_shepherd) run_workload(m_id string) {
 			}
 		}
 
-		//cmd := exec.Command("bash", "-c", "taskset -c 0 ~/mutilate/mutilate --binary -s " + l_m.ip + " --noload --agent={10.10.1.3,10.10.1.4} --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=0.25 --depth=4 --measure_depth=1 --connections=16 --measure_connections=32 --measure_qps=2000 --qps=200000 --time=10")
-		//cmd.Stdout = os.Stdout
-		//if err := cmd.Run(); err != nil { panic(err) }
-		time.Sleep(time.Second * 20)
+		cmd := exec.Command("bash", "-c", "taskset -c 0 ~/mutilate/mutilate --binary -s 10.10.1.2 --loadonly -K fb_key -V fb_value")
+		if err := cmd.Run(); err != nil { panic(err) }
+		cmd = exec.Command("bash", "-c", "taskset -c 0 ~/mutilate/mutilate --binary -s " + l_m.ip + " --noload --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=0.25 --depth=4 --measure_depth=1 --connections=16 --measure_connections=16 --measure_qps=2000 --qps=100000 --time=20")
+		cmd.Stdout = os.Stdout
+		if err := cmd.Run(); err != nil { panic(err) }
+		//time.Sleep(time.Second * 2)
 
 		for _, sheep := range(l_m.pasture) {
 			for _, log := range(sheep.logs) {
--- shep_remote_muster/shepherd/main.go
+++ shep_remote_muster/shepherd/main.go
@@ -10,9 +10,9 @@ func main() {
 //	nodes := []node{{ip: "localhost", ip_idx: 0, ncores: 1, pulse_port: 50051, log_port:50061, ctrl_port: 50071, coordinate_port: 50081},
 //			{ip: "localhost", ip_idx: 1, ncores: 1, pulse_port: 50052, log_port:50062, ctrl_port: 50072, coordinate_port: 50082}}
 
-	intlog_main(nodes)
+//	intlog_main(nodes)
 //	nop_main(nodes)
-//	bayopt_main(nodes)
+	bayopt_main(nodes)
 //	flink_main(nodes)
 }
 
