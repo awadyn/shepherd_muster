6fbf61a84cc5de23a54dc11bb6db0095e0d1fd53
awadyn <awadyn@node0.mustherd-ctrl.bayopsys-pg0.clemson.cloudlab.us>
2025-04-23 16:32:42 +0000

    removing ssh setup on global cloudlab network - keeping only ssh setup for local network

--- cloudlab_setup_ssh_conn.sh
+++ cloudlab_setup_ssh_conn.sh
@@ -8,7 +8,8 @@ for i in "${!node_list[@]}"; do
 	ssh $myself@${node_list[i]} 'if [ -f ".ssh/id_rsa.pub" ]; then echo "id_rsa.pub exists.. skipping ssh-keygen"; else ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ""; fi';
 	id_rsa=$(ssh $myself@${node_list[i]} 'cat .ssh/id_rsa.pub')
 	ip_1=$(ssh $myself@${node_list[i]} 'ifconfig | grep -B1 10.10.1 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)"')
-	ip_2=$(ssh $myself@${node_list[i]} 'ifconfig | grep -B1 128.110.96 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)"')
+#	ip_2=$(ssh $myself@${node_list[i]} 'ifconfig | grep -B1 128.110.96 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)"')
+#	ip_2=$(ssh $myself@${node_list[i]} 'ifconfig | grep -B1 130.127.133.67 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)"')
 	#echo "$id_rsa";
 	for j in "${!node_list[@]}"; do
 		if [[ $i -eq $j  ]]; then 
@@ -17,7 +18,8 @@ for i in "${!node_list[@]}"; do
 			echo "Adding ${node_list[i]} to authorized_keys of ${node_list[j]}: ";
 			ssh $myself@${node_list[j]} "if grep -q '$id_rsa' .ssh/authorized_keys; then echo '${node_list[i]} is known to this host'; else echo '$id_rsa' >> .ssh/authorized_keys; fi";
 			echo "Adding ${node_list[i]} to known_hosts of ${node_list[j]}: ";
-			ssh $myself@${node_list[j]} "ssh-keyscan -H ${node_list[i]} >> .ssh/known_hosts; ssh-keyscan -H $ip_1 >> .ssh/known_hosts; ssh-keyscan -H $ip_2 >> .ssh/known_hosts";
+#			ssh $myself@${node_list[j]} "ssh-keyscan -H ${node_list[i]} >> .ssh/known_hosts; ssh-keyscan -H $ip_1 >> .ssh/known_hosts; ssh-keyscan -H $ip_2 >> .ssh/known_hosts";
+			ssh $myself@${node_list[j]} "ssh-keyscan -H ${node_list[i]} >> .ssh/known_hosts; ssh-keyscan -H $ip_1 >> .ssh/known_hosts";
 		fi
 	done
 	echo "";
@@ -27,7 +29,8 @@ echo "Establishing first connection:"
 for i in "${!node_list[@]}"; do
 	for j in "${!node_list[@]}"; do
 		ip_1=$(ssh $myself@${node_list[j]} 'ifconfig | grep -B1 10.10.1 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)"')
-		ip_2=$(ssh $myself@${node_list[j]} 'ifconfig | grep -B1 128.110.96 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)"')
+#		ip_2=$(ssh $myself@${node_list[j]} 'ifconfig | grep -B1 130.127.133.67 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)"')
+#		ip_2=$(ssh $myself@${node_list[j]} 'ifconfig | grep -B1 128.110.96 | grep inet | grep -oP "inet \K(\d+\.\d+\.\d+\.\d+)"')
 		if [[ $i -eq $j  ]]; then 
 			continue;
 		else
@@ -35,8 +38,8 @@ for i in "${!node_list[@]}"; do
 			echo "${node_list[i]} -> ${node_list[j]} .. OK"
 			ssh $myself@${node_list[i]} "ssh $myself@$ip_1 'exit'";
 			echo "${node_list[i]} -> $ip_1 .. OK"
-			ssh $myself@${node_list[i]} "ssh $myself@$ip_2 'exit'";
-			echo "${node_list[i]} -> $ip_2 .. OK"
+#			ssh $myself@${node_list[i]} "ssh $myself@$ip_2 'exit'";
+#			echo "${node_list[i]} -> $ip_2 .. OK"
 		fi 
 	done
 	echo ""
