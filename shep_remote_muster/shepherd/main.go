2495abcf762f24ec613706a7d059a94af81d1bad
awadyn <awadyn@node0.mustherd-ctrl.bayopsys-pg0.clemson.cloudlab.us>
2025-06-12 02:54:56 +0000

    changed external network ip

--- shep_remote_muster/shepherd/main.go
+++ shep_remote_muster/shepherd/main.go
@@ -22,7 +22,7 @@ func main() {
 
 	// assume that a list of nodes is known apriori
 //	nodes := []node{{ip: "10.10.1.2", ip_idx: -1, ncores: 16, pulse_port: 50051, log_port:50061, ctrl_port: 50071, coordinate_port: 50081, optimizer_server_port: 50091, optimizer_client_port: 50101}}
-	nodes := []node{{ip: "130.127.133.33", ip_idx: -1, pulse_port: 50051, log_port:50061, ctrl_port: 50071, coordinate_port: 50081, optimizer_server_port: 50091, optimizer_client_port: 50101, resources: target_resources}}
+	nodes := []node{{ip: "130.127.133.42", ip_idx: -1, pulse_port: 50051, log_port:50061, ctrl_port: 50071, coordinate_port: 50081, optimizer_server_port: 50091, optimizer_client_port: 50101, resources: target_resources}}
 //	nodes := []node{{ip: "localhost", ip_idx: -1, ncores: 16, pulse_port: 50051, log_port:50061, ctrl_port: 50071, coordinate_port: 50081}}
 //	nodes := []node{{ip: "localhost", ip_idx: 0, ncores: 1, pulse_port: 50051, log_port:50061, ctrl_port: 50071, coordinate_port: 50081},
 //			{ip: "localhost", ip_idx: 1, ncores: 1, pulse_port: 50052, log_port:50062, ctrl_port: 50072, coordinate_port: 50082}}
