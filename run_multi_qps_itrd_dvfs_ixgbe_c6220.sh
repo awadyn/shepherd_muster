89d5d4af32648dfc761b17a03d00c8b832287f50
awadyn <awadyn@node0.load-pred-test2.mustherd-pg0.apt.emulab.net>
2025-12-06 13:00:25 -0700

    more run scripts

---
+++ run_multi_qps_itrd_dvfs_ixgbe_c6220.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+mcd_server=$1
+iface=$2
+dir=$3
+qps_=$4
+
+#for qps in 100000 200000 400000 600000 800000 1000000 1200000 1400000 1600000; do
+for qps in $qps_; do
+	mkdir -p $dir/qps_$qps/
+	for itrd in 1 2 20 50 100 150 200 250 300 350 400; do 
+		for dvfs in 0xc00 0x1100 0x1500 0x1900 0x1c00; do 
+	                echo "------------- ITRD: $itrd -- QPS: $qps -- DVFS: $dvfs";
+	
+			subdir=$dir/qps_$qps/itrd_$itrd\_dvfs_$dvfs/
+			mkdir -p $subdir
+	
+			# start memcached server
+	                ssh $mcd_server "taskset -c 0-6,8-14 ~/memcached/memcached -u nobody -t 16 -m 32G -c 8192 -b 8192 -l $mcd_server -B binary > mcd_dump 2>&1 < /dev/null &"
+			sleep 1;
+			# preload memcached database
+			taskset -c 0 ~/mutilate/mutilate --binary -s $mcd_server --loadonly -K fb_key -V fb_value
+	
+			# start mutilate agents
+	                for i in 3 4 5; do
+	                        ssh 10.10.1.$i "~/mutilate/mutilate --agentmode --threads=16 > mutilate_dump 2>&1 < /dev/null &";
+	                done
+	
+			# set itrd and flush current ixgbe logs
+			ssh $mcd_server "mkdir -p ~/ixgbe_logs/; sudo ethtool -C $iface rx-usecs $itrd; sudo wrmsr -a 0x199 $dvfs; ./flush_ixgbe_logs_c6220.sh &> /dev/null; sudo ./read_rapl_start_c6220.sh";
+			
+			# start mutilate run
+			taskset -c 0 ~/mutilate/mutilate --binary -s $mcd_server --noload --agent={10.10.1.3,10.10.1.4,10.10.1.5} --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=0.25 --depth=4 --measure_connections=4 --measure_qps=2000 --qps=$qps --time=30; 
+			
+			# get rapl readings for this run
+	                ssh $mcd_server "sudo ./read_rapl_c6220.sh; ./cat_ixgbe_logs_c6220.sh ixgbe_logs/; sudo killall memcached";
+	                for i in 3 4 5; do
+	                        ssh 10.10.1.$i "sudo killall mutilate";
+	                done
+	
+			# scp ixgbe logs
+	                echo;
+			scp -r $mcd_server:~/ixgbe_logs/* $dir/qps_$qps/itrd_$itrd\_dvfs_$dvfs/
+	                scp -r $mcd_server:~/rapl_* $dir/qps_$qps/itrd_$itrd\_dvfs_$dvfs/;
+			echo;
+	                ./compute_rapl_joules.sh $dir/qps_$qps/itrd_$itrd\_dvfs_$dvfs/rapl_pkg.txt;
+	                ./compute_rapl_joules.sh $dir/qps_$qps/itrd_$itrd\_dvfs_$dvfs/rapl_pp0.txt;
+	                echo; echo "--------------------------------"; echo;
+			#break
+		done
+		#break
+	done
+	#break
+done
+
+
+
+
+
--- run_oracle_test.sh
+++ run_oracle_test.sh
@@ -1,22 +1,54 @@
-taskset -c 0 ~/mutilate/mutilate --binary -s 10.10.1.2 --loadonly -K fb_key -V fb_value
-echo > all_mutilate_output.txt
-ssh -f 130.127.133.42 "./read_rapl_start.sh"
+mcd_server=$1
+iface=$2
 
-for qps in 400000 900000 900000 600000 200000 400000 600000 100000 600000 200000 100000 400000 900000; do
-	echo $qps;
+echo > ondemand_dvfs_lat_joules_14_cores_c6220.txt
+
+ssh $mcd_server "taskset -c 0-6,8-14 ~/memcached/memcached -u nobody -t 14 -m 32G -c 8192 -b 8192 -l $mcd_server -B binary > mcd_dump 2>&1 < /dev/null &"
+for i in 3 4 5; do
+	ssh 10.10.1.$i "~/mutilate/mutilate --agentmode --threads=16 > mutilate_dump 2>&1 < /dev/null &";
+done
+
+taskset -c 0 ~/mutilate/mutilate --binary -s 10.10.1.2 --loadonly -K fb_key -V fb_value
+sleep 1
+echo "STARTING.."
+
+ssh $mcd_server "sudo ./read_rapl_start_c6220.sh"
+#ssh $mcd_server "sudo ethtool -C $iface rx-usecs 1";
+
+for qps in 400000 800000 600000 200000 400000 100000 600000 200000 100000 800000; do
+	ssh $mcd_server "./flush_ixgbe_logs_c6220.sh &> /dev/null";
+			
 	if [[ $qps == "100000" ]]; then
-		ssh 130.127.133.42 "sudo wrmsr -a 0x199 0x1a00; sudo ethtool -C enp130s0f0 rx-usecs 300";
+		ssh $mcd_server "sudo ethtool -C $iface rx-usecs 300";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0xc00";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0xc00; sudo ethtool -C $iface rx-usecs 300";
 	elif [[ $qps == "200000" ]]; then
-		ssh 130.127.133.42 "sudo wrmsr -a 0x199 0xc00; sudo ethtool -C enp130s0f0 rx-usecs 300";
+		ssh $mcd_server "sudo ethtool -C $iface rx-usecs 300";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0xc00";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0xc00; sudo ethtool -C $iface rx-usecs 300";
 	elif [[ $qps == "400000" ]]; then
-		ssh 130.127.133.42 "sudo wrmsr -a 0x199 0x1a00; sudo ethtool -C enp130s0f0 rx-usecs 300";
+		ssh $mcd_server "sudo ethtool -C $iface rx-usecs 250";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0xc00";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0xc00; sudo ethtool -C $iface rx-usecs 250";
 	elif [[ $qps == "600000" ]]; then
-		ssh 130.127.133.42 "sudo wrmsr -a 0x199 0x1a00; sudo ethtool -C enp130s0f0 rx-usecs 200";
-	elif [[ $qps == "900000" ]]; then
-		ssh 130.127.133.42 "sudo wrmsr -a 0x199 0x1100; sudo ethtool -C enp130s0f0 rx-usecs 100";
+		ssh $mcd_server "sudo ethtool -C $iface rx-usecs 250";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0x1100";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0x1100; sudo ethtool -C $iface rx-usecs 250";
+	elif [[ $qps == "800000" ]]; then
+		ssh $mcd_server "sudo ethtool -C $iface rx-usecs 200";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0x1100";
+		#ssh $mcd_server "sudo wrmsr -a 0x199 0x1100; sudo ethtool -C $iface rx-usecs 200";
 	fi
 
-	taskset -c 0 ~/mutilate/mutilate --binary -s 10.10.1.2 --noload --agent={10.10.1.3,10.10.1.4} --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=0.25 --depth=4 --measure_depth=1 --measure_connections=512 --measure_qps=2000 --qps=$qps --time=30 >> all_mutilate_output.txt
+	taskset -c 0 ~/mutilate/mutilate --binary -s 10.10.1.2 --noload --agent={10.10.1.3,10.10.1.4,10.10.1.5} --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=0.25 --depth=4 --measure_qps=2000 --qps=$qps --time=30 >> ondemand_dvfs_lat_joules_14_cores_c6220.txt
 
-	ssh -f awadyn@130.127.133.42 "./read_rapl.sh"
+	ssh $mcd_server "sudo ./read_rapl_c6220.sh";
 done
+
+for i in 3 4 5; do
+        ssh 10.10.1.$i "sudo killall mutilate";
+done
+ssh $mcd_server "sudo killall memcached"
+
+scp -r $mcd_server:~/rapl_* .;
+
